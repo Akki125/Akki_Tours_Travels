8e54c89bef86dc86038d77fb380864cc097f7d69
Akki125 <108622209+Akki125@users.noreply.github.com>
2022-10-16 00:23:38 +0530

    Add my tours and travels site via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+
+
+
+jQuery.fn.liScroll = function(settings) {
+	settings = jQuery.extend({
+		travelocity: 0.03
+		}, settings);		
+		return this.each(function(){
+				var $strip = jQuery(this);
+				$strip.addClass("newsticker")
+				var stripHeight = 1;
+				$strip.find("li").each(function(i){
+					stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
+				});
+				var $mask = $strip.wrap("<div class='mask'></div>");
+
+				var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");								
+				var containerHeight = $strip.parent().parent().height();	//a.k.a. 'mask' width 	
+				$strip.height(stripHeight);			
+				var totalTravel = stripHeight;
+				var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye		
+				function scrollnews(spazio, tempo){
+				$strip.animate({top: '-='+ spazio}, tempo, "linear", function(){$strip.css("top", containerHeight); scrollnews(totalTravel, defTiming);});
+				}
+				scrollnews(totalTravel, defTiming);				
+				$strip.hover(function(){
+				  jQuery(this).stop();
+				},
+				function(){
+				  var offset = jQuery(this).offset();
+				  var residualSpace = offset.top + stripHeight;
+				  var residualTime = residualSpace/settings.travelocity;
+				  scrollnews(residualSpace, residualTime);
+				});			
+		});	
+};
+
+$(function(){
+    $("ul#ticker01").liScroll();
+});
+
+
+
+/*test*/
+
+jQuery(document).ready(function($) {
+	"use strict";
+	//  TESTIMONIALS CAROUSEL HOOK
+	$('#customers-testimonials').owlCarousel({
+		loop: true,
+		center: true,
+		items: 3,
+		margin: 0,
+		autoplay: true,
+		dots:true,
+		autoplayTimeout: 8500,
+		smartSpeed: 450,
+		responsive: {
+		  0: {
+			items: 1
+		  },
+		  768: {
+			items: 2
+		  },
+		  1170: {
+			items: 3
+		  }
+		}
+	});
+});
+
+/*testi*/
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        navigationText:["",""],
+        autoPlay:true
+    });
+});
